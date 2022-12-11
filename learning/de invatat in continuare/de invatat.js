ece271777f241a0e6d581926d3d2249bb30970c5
Flavius Serafim-Petru <7petrutu7@gmail.com>
2022-12-12 01:00:43 +0200

    .

---
+++ learning/de invatat in continuare/de invatat.js
@@ -0,0 +1,12 @@
+
+let myCoursesToLearn = [
+    "Learn CSS Animations",
+    "UI Design Fundamentals",
+    "Intro to CLean Code",
+    "Build more stuff",
+    "Basic javascript FTW",
+    "ES6 and beyond",
+    "APIs & async JavaScript",
+    "Server-side Javascript"
+]
+
